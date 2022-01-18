47efe3952e385ac4958a271601e5e03d086b8ae4
Shoaib Ahmed <ashoaib540918@gmail.com>
2022-01-18 14:36:28 +0530

    old fonts removed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,6 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap"
-            rel="stylesheet"
-          />
-
           <link
             rel="apple-touch-icon"
             sizes="180x180"
