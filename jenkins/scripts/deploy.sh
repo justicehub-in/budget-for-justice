444caa4c79efae2caebfd3c0abd7fd147b8dd0eb
Abhinav <a.kumar2102@gmail.com>
2022-06-14 13:46:07 +0530

    cicd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh ubuntu@3.111.39.117 << EOF
  cd /home/ubuntu/budget-for-justice 
  git pull origin data_integration
  ls
- lsof -i udp:3000 | awk '/3000/{print $2}' | xargs kill    
+ fuser -k 3000/tcp   
  npm run start &   
  exit      
  EOF
