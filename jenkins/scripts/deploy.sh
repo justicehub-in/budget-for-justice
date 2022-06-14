497e931443c9129f4925a6d7385521f3b7667a7f
Abhinav <a.kumar2102@gmail.com>
2022-06-14 14:02:32 +0530

    cicd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh  
 ssh ubuntu@3.111.39.117 << EOF 
  ls
+ fuser -k 3000/tcp 
  cd /home/ubuntu/budget-for-justice 
  git pull origin data_integration
  ls
- fuser -k 3000/tcp   
  npm run start 
  exit      
  EOF
