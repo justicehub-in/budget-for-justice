aab7139ede43c21b8743db1da007eab53f1072da
Abhinav <a.kumar2102@gmail.com>
2022-06-14 14:58:15 +0530

    cicd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,8 @@ ssh ubuntu@3.111.39.117 << EOF
  cd /home/ubuntu/budget-for-justice 
  git pull origin data_integration
  pwd
+ PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.13.0/bin
  echo $PATH
- /home/ubuntu/.nvm/versions/node/v16.13.0/bin/npm run start 
+ npm run start 
  exit      
  EOF
