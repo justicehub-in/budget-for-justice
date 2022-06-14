50dcdced6eb968657903a71da692774d5be05598
Abhinav <a.kumar2102@gmail.com>
2022-06-14 12:07:48 +0530

    ADD - kill existing node process

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,6 +4,7 @@ echo 'The following command ssh into main server and pulls and deploys'
 echo 'the build'
 set -x
 ssh ubuntu@3.111.39.117 <  cd /home/ubuntu/budget-for-justice 
+ kill $(lsof -t -i:3000)
  git pull origin data_integration     
  npm run build
  npm run start &
