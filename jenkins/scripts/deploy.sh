da214443b0bfa12a1cac279e9977f6153af78ef6
Abhinav Kumar <a.kumar2102@gmail.com>
2022-06-14 11:39:40 +0530

    ADDED - cicd deploy script

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+echo 'The following command ssh into main server and pulls and deploys'
+echo 'the build'
+set -x
+ssh ubuntu@3.111.39.117 <  cd /home/ubuntu/budget-for-justice 
+ git pull origin data_integration     
+ npm run build
+ npm run start &
+ sleep 1
+ exit      
+set +x
+
+
+echo 'The build is done and website is up'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+
