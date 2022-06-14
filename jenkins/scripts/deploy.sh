786c1a9fd04faf69e071940e18b1dba9a239eb07
Abhinav <a.kumar2102@gmail.com>
2022-06-14 13:33:25 +0530

    cicd

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
--#!/bin/sh     
-ssh ubuntu@3.111.39.117 <  cd /home/ubuntu/budget-for-justice 
+#!/usr/bin/env sh  
+ssh ubuntu@3.111.39.117 << EOF 
+ cd /home/ubuntu/budget-for-justice 
  kill $(lsof -t -i:3000)
  git pull origin data_integration      
  npm run start &   
  exit      
-EOF
+ EOF
